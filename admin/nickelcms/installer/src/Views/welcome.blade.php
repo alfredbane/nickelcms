15b32afcffa87786bbbe6ee41e1572b4a22134a6
Lovish bishnoi <lovish.bishnoi@memorres.com>
2019-09-25 21:05:49 +0530

    Added welcome screen for installer.

--- admin/nickelcms/installer/src/Views/welcome.blade.php
+++ admin/nickelcms/installer/src/Views/welcome.blade.php
@@ -1,5 +1,20 @@
-@extends('nickelcms::init')
+@extends('nickelcms::skeleton')
 
 @section('content')
-  <h3> Welcome to nickel 1.0 </h3>
+<div class="container h-100">
+  <div class="row h-100 justify-content-center align-items-center">
+    <div class="col-md-3 border--right">
+      <div class="branding">
+        <img title="nickel1.0 installer" alt="logo for nickel1.0" src="https://res.cloudinary.com/nickelcdn/image/upload/v1569413988/logo_icon_yqm20u.png" class="branding__img branding__img--130 img-responsive" />
+      </div>
+    </div>
+    <div class="col-md-5">
+      <div class="entry-text">
+        <h3>Howdy Artisan ! </h3>
+        <p class="text text--special"> Introducing NICKEL 1.0 , an open source laravel CMS. Crafted just for your upcoming creation.  </p>
+        <a class="link link--active has-background--color-gardientColorFirst" href="#">Let's go.</a>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
